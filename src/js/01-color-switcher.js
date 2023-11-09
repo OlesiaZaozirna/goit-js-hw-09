56847c109212c148e3b8c71d21ea7cdb3df1887c
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2023-11-09 22:35:14 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', colorChangeStart);
+stopButton.addEventListener('click', colorChangeStop);
+
+let intervalId = null;
+let backgroundPicture = null;
+
+function colorChangeStart() {
+  backgroundPicture = document.body.style.backgroundImage;
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+      document.body.style.backgroundImage = 'none';
+    }, 1000);
+    startButton.disabled = true;
+  }
+}
+
+function colorChangeStop() {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+  document.body.style.backgroundImage = backgroundPicture;
+}
